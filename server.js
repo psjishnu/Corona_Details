47da3c840c3dd8d49fe64475d2a20c4e51b36c4b
Jishnu P Sreekumar <33541774+psjishnu@users.noreply.github.com>
2020-03-17 15:40:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+const axios = require("axios");
+var cheerio = require("cheerio");
+const rp = require('request-promise');
+
+
+var port = process.env.PORT || 8000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+
+
+	res.render('index',{result:""});
+});
+app.get('/name', callName); 
+  
+function callName(req, res) { 
+      
+
+    var spawn = require("child_process").spawn; 
+
+    var process = spawn('python',["./corona.py", 
+                            req.query.firstname, 
+                            req.query.lastname] ); 
+  
+
+	msg="";
+    process.stdout.on('data', function(data) { 
+		sum=(data.toString());
+		if(req.query.lastname=="deaths")
+		{
+			msg=sum+" people died in "+req.query.firstname;
+		}
+		if(req.query.lastname=="latest")
+		{
+			msg=sum+" people are affected with Corona in "+req.query.firstname
+		}
+		if(req.query.lastname=="confirmed")
+		{
+			msg="There are "+sum+" confirmed cases of Corona in "+req.query.firstname;
+		}
+		if(req.query.lastname=="recovered")
+		{
+			msg=sum+" people have been recoverd from Corona in "+req.query.firstname;
+		}
+		res.render('index',{result:msg}); 
+    } ) 
+} 
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});

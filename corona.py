e0aacf48de7e9478436342d8db4a366447bdbdd2
psjishnu <psjishnu13@gmail.com>
2020-03-17 15:44:04 +0530

    commit

---
+++ corona.py
@@ -0,0 +1,68 @@
+import sys 
+#This will not run on online IDE 
+import requests 
+from bs4 import BeautifulSoup 
+import json
+
+x=(sys.argv[1])
+y=(sys.argv[2])
+
+country=x
+URL = "https://coronavirus-tracker-api.herokuapp.com/a1"
+URL = URL.replace("a1",y)
+r = requests.get(URL) 
+soup = BeautifulSoup(r.content, 'html5lib') 
+p=soup.prettify()
+p=p.replace("<body>",'')
+p=p.replace("</body>",'')
+p=p.replace("<head>",'')
+p=p.replace("</head>",'')
+p=p.replace("<html>",'')
+p=p.replace("</html>",'')
+
+y = json.loads(p)
+y = json.dumps(y)
+
+y= json.loads(y)
+y=str(y["locations"])
+y=json.dumps(y)
+x=0
+length=len(country)
+x=len(y)
+i=0
+i1=0
+j=0
+j1=0
+sum=0
+for i in range (0,x) :
+    if(y[i]==country[0]):
+        j=i
+        if(y[j:j+length:1]==country and y[j+length]=='\'' and y[j-1]=='\''):
+            for i1 in range(i,x):
+                if(y[i1]=='l'):
+                    j1=i1
+                    if(y[j1:j1+6:1]=="latest"):
+                        l1=j1+9
+                        while(y[l1]!=' '):
+                            l1=l1+1
+                        sum=sum+int(y[j1+9:l1-1])
+                        break
+print(sum)
+
+
+#print(soup.prettify()) 
+
+
+"""
+
+# Takes first name and last name via command 
+# line arguments and then display them 
+print("Output from Python") 
+
+print("First name: " + sys.argv[1]) 
+print("Last name: " + sys.argv[2]) 
+x=int(sys.argv[1])+int(sys.argv[2])
+print("SUM IS :",x)
+ """
+
+# save the script as hello.py 
---
+++ public/index.js
@@ -0,0 +1,21 @@
+function fun1(){
+    var name1=document.getElementById("id1").value;
+    var rd1 = document.getElementById("case1");
+    var rd2 =document.getElementById("case2");
+    var rd3 = document.getElementById("case3");
+    var rd4 =document.getElementById("case4");
+    if(rd1.checked==true){
+        name2=rd1.value;
+    }
+    else if(rd2.checked==true){
+        name2=rd2.value;
+    }
+    else if(rd3.checked==true){
+        name2=rd3.value;
+    }
+    else
+        name2=rd4.value;
+    console.log(name2);
+    window.location.href='\\name?firstname='+name1+'&lastname='+name2;
+
+}
